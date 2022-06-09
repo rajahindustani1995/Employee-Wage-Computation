8721f32e9a8f1ac9ff4504c3b2f2e7f49bbba502
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-09 14:23:18 +0530

    Monthly Wage Added

---
+++ EmployeeWageComputation/EmpDailyWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmpDailyWage
+    {
+        public int EmpPresent = 1;
+        public int Wage_Per_Hour = 20;
+        public int Full_Day_Hour = 8;
+
+
+        public void DailyWage()
+        {
+            Random EmpCheck = new Random();
+            int value = EmpCheck.Next(0, 2);
+
+            if (value == EmpPresent)
+            {
+                int Daily_Wage = Wage_Per_Hour * Full_Day_Hour;
+                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee Daily Wage is = " + Daily_Wage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee Daily Wage is = 0");
+            }
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,6 +6,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmpDailyWage obj = new EmpDailyWage();
+            obj.DailyWage();
 
         }
     }
