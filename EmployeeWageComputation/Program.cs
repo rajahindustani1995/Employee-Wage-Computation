1584b09afa5bee295411c8497c94a2c6956d89be
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-09 11:50:23 +0530

    First Commit

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageComputation
+{
+
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+        }
+    }
+}
