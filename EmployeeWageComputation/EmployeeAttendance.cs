4c2f16abe1004b9bc086eaa613d0b3b103887c89
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-09 12:01:22 +0530

    Employee Attendance Added

---
+++ EmployeeWageComputation/EmployeeAttendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeAttendance
+    {
+        public int EmpPresent = 1;
+
+        public void CheckEmpPresentAbsent()
+        {
+            Random EmpCheck = new Random();
+            int value = EmpCheck.Next(0, 2);
+
+            if (value == EmpPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-
+            EmployeeAttendance prg = new EmployeeAttendance();
+            prg.CheckEmpPresentAbsent();
         }
     }
 }
