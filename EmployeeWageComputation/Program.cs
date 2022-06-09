e342bee10a7d5960195c6343346be0d86f8756d2
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-09 15:27:18 +0530

    Switch Case Added

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-
+            UsingSwitchCase abc = new UsingSwitchCase();
+            abc.CalculateWage();
         }
     }
 }
